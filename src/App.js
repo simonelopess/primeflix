950c1502d8451f90a34c9c4db27c6f4dac3a3160
Simone Lopes <sisilopes20@gmail.com>
2023-04-11 16:09:30 +0100

    get movies from api using params

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+
 import RoutesApp from "./routes";
 
 function App() {
+
+
   return (
     <>
       <RoutesApp />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from 'react-router-dom';
+import './home.css'
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        setLoading(true);
+        const response = await api.get("/movie/now_playing", {
+          params: {
+            api_key: "01c1e9c3bc64272ab9e34515f52aedf5",
+            language: "pt-BR",
+            page: 1,
+          },
+        });
+        setLoading(false);
+        setMovies(response.data.results.slice(0, 10));
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getMovies();
+  }, []);
+
   return (
-    <div>Página Home</div>
-  )
+    <div className="container">
+      <div className="list-movies">
+        {movies &&
+          movies.map((movie) => (
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <Link to={`/filme/${movie.id}`}>Acessar</Link>
+            </article>
+          ))}
+      </div>
+    </div>
+  );
 }
